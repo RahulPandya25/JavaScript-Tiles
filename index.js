22ffa0a2cda2276c69750d8824bc308afeb298f8
Rahul <rahul25796@gmail.com>
2019-08-27 23:21:33 +0530

    testing dynamic grid

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-var mainClass = document.querySelector(".main");
+const fillGrid = (x, y, blockSize, numOfBlocks, color) => {
+  const container = document.querySelector(".main");
+  const test = Array(numOfBlocks).keys();
+  for (var i of test) {
+    let markup = `<div id="card ${i}" 
+                                   style="grid-column: ${Math.floor(
+                                     Math.random() * y
+                                   ) + 1} / span ${blockSize}; 
+                                          grid-row: ${Math.floor(
+                                            Math.random() * x
+                                          ) + 1} / span ${blockSize};
+                                          background-color: ${color};
+                                          border: 1px black solid;">${i}</div>`;
+    container.innerHTML += markup;
+  }
+};
 
-console.log(mainClass);
+fillGrid(4, 5, 2, 20, "lightgreen");
