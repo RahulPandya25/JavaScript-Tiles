5b66168615cfa44dce49b31b1a670d172a636de7
Rahul <rahul25796@gmail.com>
2019-09-08 20:11:48 +0530

    made responsive

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var clickThis;
-var rows = 3;
-var columns = 3;
+var rows = 4;
+var columns = 5;
 
 const fillGrid = (x, y, blockSize, color) => {
   const container = document.querySelector("main");
@@ -44,4 +44,43 @@ var removeClickClass = function() {
   });
 };
 
-fillGrid(rows, columns, 1, "lightblue");
+var updateGridMatrix = function(row, col) {
+  // updating css var
+  document.documentElement.style.setProperty("--rowNum", row);
+  document.documentElement.style.setProperty("--colNum", col);
+  // updating js var
+  rows = row;
+  columns = col;
+};
+
+var refillGrid = function() {
+  // remove existing grid
+  var main = document.querySelector("main");
+  var elements = main.getElementsByClassName("card");
+  while (elements[0]) {
+    elements[0].parentNode.removeChild(elements[0]);
+  }
+  fillGrid(rows, columns, 1, "white");
+};
+
+function myFunction(x) {
+  if (x.matches) {
+    // If media query matches
+    // update css/js variables
+    updateGridMatrix(3, 3);
+
+    console.log("if");
+  } else {
+    // update css/js variables
+    updateGridMatrix(4, 5);
+    console.log("else");
+  }
+  // remove old grid and fill new one
+  refillGrid();
+}
+
+var x = window.matchMedia("(max-width: 786px)");
+myFunction(x); // Call listener function at run time
+x.addListener(myFunction); // Attach listener function on state changes
+
+fillGrid(rows, columns, 1, "white");
